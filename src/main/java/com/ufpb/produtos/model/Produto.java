2b3c4861559c240852b7f200e2af936bda49b591
Alexandre <alexandre.f.cesar@gmail.com>
2020-02-26 23:11:39 -0300

    inserir produtos

--- src/main/java/com/ufpb/produtos/model/Produto.java
+++ src/main/java/com/ufpb/produtos/model/Produto.java
@@ -5,6 +5,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 
 
 @Entity
@@ -14,11 +16,13 @@ public class Produto {
 	@Id
 	@GeneratedValue(strategy= GenerationType.IDENTITY)
 	private Long id;
-     
+    @NotEmpty
+    @Size(max=90)
     @Column(name = "nomeDoProduto")
 	private String nome;
 	
-
+    @NotEmpty
+    @Size(max=100)
 	private Double valor;
 
 	public Long getId() {
